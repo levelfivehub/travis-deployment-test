bd2a943c67ce109fa7d3e864d1e3a729322fa788
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-15 21:26:29 +0100

    Deploy to production test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R ./ /public_html --ignore-time"
+ssh-keygen -p -P "$DEPLOY_PASS" -N '' -f deploy_key
+eval "$(ssh-agent -s)" && ssh-add deploy_key
+ssh nyeuk@nyeuk.com << EOF
+cd public_html
+if [ -d ".git" ]; then
+    git pull
+else
+    git init
+    git remote add origin git@github.com:levelfivehub/travis-deployment-test.git
+    git checkout "$TRAVIS_BRANCH"
+    git reset --hard HEAD
+fi
+EOF
