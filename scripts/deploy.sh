207b9ddfd7c8dd1381d2b527ba31a658bed32899
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-16 19:33:15 +0100

    testing directory structure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,5 @@ chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to
 ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
 
 ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
-cd public_html
-if [ -d ".git" ]; then
-    git pull
-else
-    git init
-    git remote add origin git@github.com:levelfivehub/travis-deployment-test.git
-    git checkout "$TRAVIS_BRANCH"
-    git reset --hard HEAD
-fi
+ls -asl
 EOF
