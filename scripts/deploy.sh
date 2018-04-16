80de277471090c52378550ee8940871bdf85d172
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-16 19:39:41 +0100

    Removed - Rebuilt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,13 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to the private key
 ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
 
-ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
+echo "Resetting workspace directory"
 
+ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
+    cd public_html
+    rm -rf *
 EOF
+
+echo "Copying over new build"
 
 scp -r "$TRAVIS_BUILD_DIR"/* nyeuk@nyeuk.com:/home/nyeuk/public_html
