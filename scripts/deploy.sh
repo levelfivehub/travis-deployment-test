514e419d02a5a320da077453216b86286fb6ccae
Gaurav Malhotra <7773587+levelfivehub@users.noreply.github.com>
2018-04-16 19:59:29 +0100

    Release 1.0 (#1)
    * Updated travis build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-ssh-keygen -p -P "$DEPLOY_PASS" -N '' -f deploy_key
-eval "$(ssh-agent -s)" && ssh-add deploy_key
-ssh nyeuk@nyeuk.com << EOF
-cd public_html
-if [ -d ".git" ]; then
-    git pull
-else
-    git init
-    git remote add origin git@github.com:levelfivehub/travis-deployment-test.git
-    git checkout "$TRAVIS_BRANCH"
-    git reset --hard HEAD
+DIRECTORY="public_html"
+
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    DIRECTORY="preview"
 fi
+
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to the private key
+ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
+
+echo "Resetting workspace directory"
+
+ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
+    cd "$DIRECTORY"
+    rm -rf *
 EOF
+
+echo "Copying over new build"
+
+scp -r "$TRAVIS_BUILD_DIR"/* nyeuk@nyeuk.com:/home/nyeuk/"$DIRECTORY"
