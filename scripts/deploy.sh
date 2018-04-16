4dcdc5d245b5bf0e23387dc422e1cc562d216cf0
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-16 19:36:50 +0100

    SCP not GIT

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,7 @@ chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to
 ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
 
 ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
-ls -asl
+
 EOF
+
+scp -r "$TRAVIS_BUILD_DIR"/* nyeuk@nyeuk.com:/home/nyeuk/public_html
