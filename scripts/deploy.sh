589e714ca5b7d98b72195b73ea88ed9ef28e990a
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-16 19:49:51 +0100

    Added in multiple directory options

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+DIRECTORY="public_html"
+
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    DIRECTORY = "preview"
+fi
+
+
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to the private key
 ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
@@ -6,10 +13,10 @@ ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to
 echo "Resetting workspace directory"
 
 ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
-    cd public_html
+    cd "$DIRECTORY"
     rm -rf *
 EOF
 
 echo "Copying over new build"
 
-scp -r "$TRAVIS_BUILD_DIR"/* nyeuk@nyeuk.com:/home/nyeuk/public_html
+scp -r "$TRAVIS_BUILD_DIR"/* nyeuk@nyeuk.com:/home/nyeuk/"$DIRECTORY"
