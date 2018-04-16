169997158b009d5eddf7aec051b9bed96cb24030
Gaurav Malhotra <gaurav@level5websites.com>
2018-04-16 19:30:48 +0100

    Added in build directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ../.travis/nyeuk_travis_deploy # Allow read access to the private key
-ssh-add ../.travis/nyeuk_travis_deploy # Add the private key to SSH
+chmod 600 "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Allow read access to the private key
+ssh-add "$TRAVIS_BUILD_DIR"/.travis/nyeuk_travis_deploy # Add the private key to SSH
 
 ssh -o StrictHostKeyChecking=no nyeuk@nyeuk.com << EOF
 cd public_html
